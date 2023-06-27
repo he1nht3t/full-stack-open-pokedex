34b42414d329f068274a4c2c7ab501da2c2765bb
Heinhtet Aung <he1nh3t@MacBook-Air.local>
2023-06-27 11:47:42 +0630

    added /health route

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5001
 
-app.get('/version', (_, res) => {
-  res.send('0.0.2')
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.use(express.static('dist'))
